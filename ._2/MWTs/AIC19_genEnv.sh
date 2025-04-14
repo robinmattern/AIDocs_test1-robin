5f733e36d7a37191f8f3768059249d86685cc061
Robin Mattern <robin.mattern@gmail.com>
2025-04-13 21:26:37 -0400

    .(50413.09_Today's changes

--- ._2/MWTs/AIC19_genEnv.sh
+++ ._2/MWTs/AIC19_genEnv.sh
@@ -26,8 +26,8 @@ for i in "${!mArray[@]}"; do
     mArray[$i]=$(echo "${mArray[$i]}" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
 done
 
-   aTests="${mArray[5]}-tests"; if [ "${mArray[5]}" == "1" ]; then aTests="1-test"; fi 
    aModel="${mArray[1]/:/;}"
+   aTests="${mArray[5]}-tests"; if [ "${mArray[5]}" == "1" ]; then aTests="1-test"; fi 
    aTitle="${aModel}_${aTests}"
    }
 # -----------------------------------------------------------------
@@ -43,8 +43,8 @@ function mergeVars() {
 #done
 
 # Read the template file
-template_file="$1"
-output_file="$2"
+    template_file="$1"
+    output_file="$2"
 
 if [ ! -f "$template_file" ]; then
     echo "Template file not found: $template_file"
@@ -52,7 +52,8 @@ if [ ! -f "$template_file" ]; then
 fi
 
 # Read the template into a variable
-aBodyText=$(cat "$template_file")
+    aBodyText=$(cat "$template_file")
+    mArray[0]="${mArray[0]:4:4}"  # aTestId=
 
 # Replace placeholders with array elements
 # TestId,      Model,              CTX_SIZE, SysPmtCd, Temp, Tests, DOCs, URLs, USPF, UUPF, Sections
@@ -62,27 +63,32 @@ aBodyText=$(cat "$template_file")
     placeholder="{CTX_Size}";  replacement="${mArray[2]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
     placeholder="{SysPmtCd}";  replacement="${mArray[3]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
     placeholder="{Temp}";      replacement="${mArray[4]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
+    placeholder="{Runs}";      replacement="${mArray[6]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
     placeholder="{Tests}";     replacement="${mArray[5]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
-    placeholder="{DOCs}";      replacement="${mArray[6]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
-    placeholder="{URLs}";      replacement="${mArray[7]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
-    placeholder="{USPF}";      replacement="${mArray[8]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
-    placeholder="{UUPF}";      replacement="${mArray[9]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
+    placeholder="{DOCs}";      replacement="${mArray[7]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
+    placeholder="{URLs}";      replacement="${mArray[8]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
+    placeholder="{USPF}";      replacement="${mArray[9]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
+    placeholder="{UUPF}";      replacement="${mArray[10]}";  aBodyText="${aBodyText//$placeholder/$replacement}"
     placeholder="{Sections}";  replacement="${part2}";       aBodyText="${aBodyText//$placeholder/$replacement}"
-    placeholder="{Title}";     replacement="${aTitle}";       aBodyText="${aBodyText//$placeholder/$replacement}"
+    placeholder="{Title}";     replacement="${aTitle}";      aBodyText="${aBodyText//$placeholder/$replacement}"
+    placeholder="{Cnt}";       replacement="${mArray[5]}";   aBodyText="${aBodyText//$placeholder/$replacement}"
+    placeholder="{SysPrompt}"; replacement="${aSysPrompt}";  aBodyText="${aBodyText//$placeholder/$replacement}"
 
     
 #   echo "  Using the following settings:" 
-    echo "    Model:           ${mArray[1]}"
-    echo "    CTX_Size:        ${mArray[2]}"
-    echo "    Temperature:     ${mArray[4]}"
-    echo "    SysPmt Code:     ${mArray[3]}"
-    echo "    Do Doc Search:   $( YorN ${mArray[6]} )"
-    echo "    Do Web Search:   $( YorN ${mArray[7]} )"
-    echo "    Use SysPmt File: $( YorN ${mArray[8]} )"
-    echo "    Use UsrPmt File: $( YorN ${mArray[9]} )"
-    echo "    Test Title:      ${aTitle}"
-    echo "    Test Runs:       ${mArray[5]}"
-    echo "    Sections:        ${part2}"
+    echo "    1. Model:           ${mArray[1]}"
+    echo "    2. CTX_Size:        ${mArray[2]}"
+    echo "    3. Temperature:     ${mArray[4]}"
+    echo "    4. SysPmt Code:     ${mArray[3]}"
+    echo "    5. Do Doc Search:   $( YorN ${mArray[7]} )"
+    echo "    6. Do Web Search:   $( YorN ${mArray[8]} )"
+    echo "    7. Use SysPmt File: $( YorN ${mArray[9]} )"
+    echo "    8. Use UsrPmt File: $( YorN ${mArray[10]} )"
+    echo "    9. Test Title:      ${aTitle}"
+    echo "   10. UsrPrompt Runs:  ${mArray[6]}"
+    echo "   10. SysPrompt Tests: ${mArray[5]}"
+    echo "   11. First Run Id:    t${aTest}.01"
+    echo "   12. Sections:        ${part2}"
     echo ""
     echo "$aBodyText" > "$output_file"  # Write the result to the output file
 #   echo "  The .env file saved to: $output_file"
@@ -97,14 +103,18 @@ aBodyText=$(cat "$template_file")
 #  echo "  aModel: '${mArray[1]}', nTests: '${mArray[5]}'"
    aDestFile=".env_${aApp}_${aTest}_${aTitle}.txt" 
    echo "  Creating the .env test file with the following parameters:" 
-   mergeVars ".env_s11-template.txt" "${aDestFile}"
+   mergeVars ".env_${aApp}-template.txt" "${aDestFile}"
    echo "  Saved the .env test file: ${aDestFile}." 
  else 
-   aTestParms="$( cat "${aApp}_model-tests.txt" | awk '/'${aTest}'/ { print }' )"
+   
+# echo "cat \"${aApp}_system-prompts.txt\" | awk '/${aApp}, t00${aTest:2:1}/'"  
+   aSysPrompt="$( cat "${aApp}_system-prompts.txt" | awk '/'"${aApp}, t00${aTest:2:1}"'/ { aPrompt = substr( $0, 32 ); sub( /[" ]+$/, "", aPrompt ); print aPrompt }' )"
+#  echo "--- aTest: ${aTest}, aSysprompt: '${aSysPrompt}'"; exit 
+   aTestParms="$( cat "${aApp}_model-tests.txt"    | awk '/'${aTest}'/ { print }' )"
    echo "  Creating an .env file with the following parameters:" 
    splitParms "${aTestParms}"
-   mergeVars ".env_s11-template.txt" ".env"
-   echo "  Saved .env file for test run ${aTest}." 
+   mergeVars ".env_${aApp}-template.txt" ".env"
+   echo "  Saved .env file for test run t${aTest}." 
    fi 
 
 
--- server1/s11_search-app/run-tests.sh
+++ server1/s11_search-app/run-tests.sh
@@ -1,22 +1,76 @@
 #!/bin/bash
         aApp=s11 
-        aThreadId=${1:0:4}; bCopied=0
-                                              
-if [ "${aThreadId/t}" != "${aThreadId}" ]; then ../../._2/MWTs/AIC19_genEnv.sh ${aApp} ${aThreadId/t}; bCopied=1; shift; else  
-if [ "${aThreadId}" == "t010" ]; then cp -p ".env_${aApp}_t010_*.txt" .env; bCopied=1; shift; fi 
-if [ "${aThreadId}" == "t020" ]; then cp -p ".env_${aApp}_t002_*.txt" .env; bCopied=1; shift; fi 
-if [ "${aThreadId}" == "t030" ]; then cp -p ".env_${aApp}_t003_*.txt" .env; bCopied=1; shift; fi;   
-if [ "${aThreadId}" == "t040" ]; then cp -p ".env_${aApp}_t004_*.txt" .env; bCopied=1; shift; fi; fi  
+        aTests="t010,t020,t030,t040"
+        aArgs="$1,$2,$3,$4,$5,$6,$7,$8,$9"; aArgs=${aArgs//,,/}; s=""
 
-if [ "$1" == "" ] && [ "${bCopied}" ==  "0" ]; then 
-   echo ""
-   echo "  Run any of the following tests:"
-         ls -l .env_${aApp}_* | awk '{ print "    bash run-tests.sh  " substr($9,10,4) "  # " $9 }' 
-   if [ "${OS:0:3}" != "Win" ]; then echo ""; fi 
-   exit 
+   if [ "$1" == "" ]; then 
+        echo ""
+        echo "  Run any of the following tests:"
+        echo "    bash run-tests.sh  t0##  # .env_s11_t0##_model_#-tests.txt"  
+        ls -l .env_${aApp}_* | awk '{ print "    bash run-tests.sh  " substr($9,10,4) "  # " $9 }' 
+        if [ "${OS:0:3}" != "Win" ]; then echo ""; fi 
+        exit 
+    fi 
+
+        IFS=',' read -ra mArgs <<< "${aArgs}"; nTests=0
+    for aTest in "${mArgs[@]}"; do
+     if [[ "${aTest}" =~ ^t[0-9]{3}$ ]]; then (( nTests++ ))
+      else 
+        echo -e "\n* Invalid Test Id. Please use one of the following:"
+        echo      "    t010 t020 t030 t040 t041 or more: t010,t020,t030"
+        if [ "${OS:0:3}" != "Win" ]; then echo ""; fi 
+        exit 
+      fi
+    done
+    s="s"; if [ "${nTests}" == "1" ]; then s=""; fi 
+    echo -e "\n  Running test${s} for: '${aArgs}'."; # exit 
+    shift 
+# -------------------------------------------------------------------
+aAWKscr='
+function getFld( aRow ){ 
+         n=split( aRow, aFld, "," ); 
+    for (i=1; i<=n; i++) { 
+     if (aFld[i] ~ /^t[0-9]{3}$/) { 
+         aThreadId=aFld[i]; 
+         return aThreadId; 
+         } 
+     } 
+ } )
+'
+aAWKscr='
+function getFld( aRow ) { 
+         split( aRow, mVar, "=" ); aVar = mVar[2]; sub( /#.+/, "", aVar ); # print " -- " aVar;  
+         sub( /^[" ]+/, "", aVar ); sub( /[" ]+$/, "", aVar );             # print " -- [" aVar "]"; 
+  return aVar 
+         }
+   /^[#]/ { next }
+   /OLLAMA_MODEL_NAME/    { print "    1.  Model:           "  getFld( $0 )} 
+   /CTX_SIZE/             { print "    2.  CTX_Size:        "  getFld( $0 ) } 
+   /TEMPERATURE/          { print "    3.  Temperature:     "  getFld( $0 ) } 
+   /SYS_PROMPT_CD/        { print "    4.  SysPmt Code:     "  getFld( $0 ) } 
+   /USE_DOCS/             { print "    5.  Do Doc Search:   " (getFld( $0 ) ? "Yes" : "No") }
+   /USE_URLS/             { print "    6.  Do Web Search:   " (getFld( $0 ) ? "Yes" : "No") }
+   /USE_SYS_PROMPTS_FILE/ { print "    7.  Use SysPmt File: " (getFld( $0 ) ? "Yes" : "No") }
+   /USE_USR_PROMPTS_FILE/ { print "    8.  Use UsrPmt File: " (getFld( $0 ) ? "Yes" : "No") }
+   /SESSION_TITLE/        { print "    9.  Test Title:      "  getFld( $0 ) } 
+   /USR_RUN_COUNT/        { print "   10.  UsrPrompt Runs:  "  getFld( $0 ) } 
+   /SYS_RUN_COUNT/        { print "   11.  SysPrompt Tests: "  getFld( $0 ) } 
+   /SESSION_ID/           { print "   12.  First Run Id:    "  getFld( $0 ) ".01" }
+   /SHOW_SECTIONS/        { print "   13.  Sections:        "  getFld( $0 ) }
+'         
+    for aThreadId in "${mArgs[@]}"; do
+#       aThreadId=${1:0:4}; bCopied=0
+                                              
+if [ "${aThreadId:3:1}" != "0" ]; then ../../._2/MWTs/AIC19_genEnv.sh ${aApp} ${aThreadId/t}; else  
+if [ "${aThreadId}"  == "t010" ]; then cp -p .env_${aApp}_t010_*.txt .env; fi 
+if [ "${aThreadId}"  == "t020" ]; then cp -p .env_${aApp}_t020_*.txt .env; fi 
+if [ "${aThreadId}"  == "t030" ]; then cp -p .env_${aApp}_t030_*.txt .env; fi;   
+if [ "${aThreadId}"  == "t040" ]; then cp -p .env_${aApp}_t040_*.txt .env; fi; fi  
+
+if [ "${aThreadId:3:1}" == "0" ]; then 
+   echo -e "\n  Using an env file with the following parameters:"
+   cat .env | awk "${aAWKscr}" | sort -k1,1
    fi 
 
-   node search_u2.04.mjs  "$@"
-
-
-
+   node search_u2.05.mjs  "$@"
+   done 
