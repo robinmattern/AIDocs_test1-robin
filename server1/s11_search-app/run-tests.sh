1836c73c03cd350cb00eb6105c423c455fc4946d
Robin Mattern <robin.mattern@gmail.com>
2025-04-19 17:30:01 -0400

    .(50419.07_Keep Test run group 040

--- server1/s11_search-app/run-tests.sh
+++ server1/s11_search-app/run-tests.sh
@@ -7,7 +7,7 @@
 #    export LOGGER=
 #    export LOGGER="log"   # no workie
 #    export LOGGER="inputs"
-#    export LOGGER="log,inputs"
+     export LOGGER="log,inputs"
 
      export DOIT="1"
      export DEBUG="0"
@@ -15,10 +15,6 @@
      export PC_CODE="rm231d"
 
      bash   ../../._2/MWTs/AIC15_runTests_u1.02.sh  "$@";  if [ $? -ne 0 ]; then exit 1; fi
-#    shift 
- 
-#    node search_u2.05.mjs  "$@" 
-#    node   search_u2.05.mjs    # run by AIC15_runTests_u1.02.sh
 
   if [ "${LOGGER}" == "log,inputs" ]; then 
      echo -e "\n-----------------------------------------------------------"
