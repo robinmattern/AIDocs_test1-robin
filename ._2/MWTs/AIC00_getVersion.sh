a0c397c3d8cd61a0b390bfa08c5421f23d883ed1
Robin Mattern <robin.mattern@gmail.com>
2025-04-20 14:51:42 -0400

    .(50420.08_Minor fixes; version u2.05.131

--- ._2/MWTs/AIC00_getVersion.sh
+++ ._2/MWTs/AIC00_getVersion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -e "\n  AIDocs AItestR4u u2.05.131"
+echo -e "\n  AIDocs AItestR4u u2.05.132"
 if [ "${OS:0:3}" != "Win" ]; then echo ""; fi 
 
--- server1/s12_search-web-app/run-tests.sh
+++ server1/s12_search-web-app/run-tests.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
         
-     if [ "$1" == "s12" ]; then shift; fi 
+     if [ "$1" == "s11" ]; then shift; fi 
+     if [ "${1:0:3}" == "ver" ]; then "../../._2/MWTs/AIC00_getVersion.sh"; exit; fi    # .(50420.01.4)
+     aRun_Tests="../../._2/MWTs/AIC15_runTests_u1.02.sh"
 
-     export APP=s12
+     export APP=s11 
 
 #    export LOGGER=
 #    export LOGGER="log"   # no workie
 #    export LOGGER="inputs"
-#    export LOGGER="log,inputs"
+     export LOGGER="log,inputs"
 
      export DOIT="1"
      export DEBUG="0"
  
-#    export PC_CODE="pc001p"
-     export PC_CODE="rm231d"
-#    export PC_CODE="bt04st"
+     export PC_CODE=""
 
-#    export ENV_TEMPLATE="../../.env_${APP}-template_${PC_CODE}.txt"
-     export ENV_TEMPLATE=".env_${APP}-template_${PC_CODE}.txt"
-#    export ENV_TEMPLATE=".env_${APP}-template_${PC_CODE}.txt"
-
-     export DEBUG="0"
-     bash   ../../._2/MWTs/AIC15_runTests_u1.02.sh  "$@";  if [ $? -ne 0 ]; then exit 1; fi
-#    shift 
- 
-#    node search_u2.05.mjs  "$@" 
-#    node   search_u2.05.mjs    # run by AIC15_runTests_u1.02.sh
-
-  if [ "${LOGGER}" == "log,inputs" ]; then 
-     echo -e "\n-----------------------------------------------------------"
-     fi
-     if [ "${OS:0:3}" != "Win" ]; then echo ""; fi 
-
+     bash  "${aRun_Tests}"  "$@";  if [ $? -ne 0 ]; then exit 1; fi
