476b043639d6b0f5cc7aa268af69bf0084a3f7d3
Robin Mattern <robin.mattern@gmail.com>
2025-04-27 18:11:46 -0400

    .(50427.01_Fix wrong script .ext in run-tests.sh

--- server1/s11_search-app/run-tests.sh
+++ server1/s11_search-app/run-tests.sh
@@ -3,6 +3,7 @@
      if [ "$1" == "s11" ]; then shift; fi 
      if [ "${1:0:3}" == "ver" ]; then "../../._2/MWTs/AIC00_getVersion.sh"; exit; fi    # .(50420.01.4)
      aRun_Tests="../../._2/MWTs/AIC15_runTests_u1.02.sh"
+     export SEARCH_SCRIPT="search_u2.06.mjs"
 
      export APP=s11 
 
--- server1/s12_search-web-app/run-tests.sh
+++ server1/s12_search-web-app/run-tests.sh
@@ -3,7 +3,7 @@
      if [ "$1" == "s12" ]; then shift; fi 
      if [ "${1:0:3}" == "ver" ]; then "../../._2/MWTs/AIC00_getVersion.sh"; exit; fi    # .(50420.01.4)
      aRun_Tests="../../._2/MWTs/AIC15_runTests_u1.02.sh"
-     export SEARCH_SCRIPT="search_u2.06.sh"
+     export SEARCH_SCRIPT="search_u2.06.mjs"
 
      export APP=s12 
 
@@ -15,6 +15,6 @@
      export DOIT="1"
      export DEBUG="0"
  
-     export PC_CODE="cn0g0p"
+     export PC_CODE=""
 
      bash  "${aRun_Tests}"  "$@";  if [ $? -ne 0 ]; then exit 1; fi
