83aa5a8d5dcafd28a706ffcbb7ae56cbb34c41c5
Robin Mattern <robin.mattern@gmail.com>
2025-05-13 09:33:33 -0400

    .(50513.01_ Bump version to u2.09.137

--- ._2/MWTs/AIC00_getVersion.sh
+++ ._2/MWTs/AIC00_getVersion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-echo -e "\n  AIDocs - AI.testR.4u  Ver: u2.08.136"
+aDate="$(date +'%B %d, %Y %l:%M%p')"; aDate="${aDate/AM/a}"; aDate="${aDate/PM/p}"
+echo -e "\n  AIDocs - AI.testR.4u  Ver: u2.09.137  (${aDate})"
 if [ "${OS:0:3}" != "Win" ]; then echo ""; fi 
