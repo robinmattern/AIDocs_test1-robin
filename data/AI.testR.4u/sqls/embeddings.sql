a4665e130a96618bd0bc1dc0792d2bcaa82cd129
Robin Mattern <robin.mattern@gmail.com>
2025-06-05 10:09:11 -0400

    .(50605.01_Delete ChromaDB remnants

--- data/AI.testR.4u/sqls/embeddings.sql
+++
@@ -1,2 +0,0 @@
- 
-  SELECT id, seq_id, embedding_id, created_at FROM embeddings where id = 1390
--- data/AI.testR.4u/sqls/segments.sql
+++
@@ -1,6 +0,0 @@
-  SELECT min(created_at) as created_at_begin, '...' || substr(collections.id,20,17) as collection_id, name, segment_id, count() segments, max(created_at) 
-    FROM collections, embeddings, segments 
-   WHERE collections.id = segments.collection 
-     AND segments.id    = embeddings.segment_id 
-GROUP BY collections.id, name, segment_id  
-ORDER BY min(created_at)
