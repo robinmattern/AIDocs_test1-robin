896248cf13bf9e6c7e820aee21a9d70b03149a90
Robin Mattern <robin.mattern@gmail.com>
2025-06-11 10:33:01 -0400

    .(50611.01_Is it or is it not imported

--- install-aitestr.sh
+++ install-aitestr.sh
@@ -14,6 +14,8 @@
 #  aRepo="$1"; if [ "$1" == "" ]; then aRepo="AIDocs_demo1"; fi                         # .(50402.15.5)
 
    aAppDir="$1"; aAccount="$2"; # echo -e "\n  \$1: '$1', \$2: '$2'"; aAppDir="$1"; aAccount="$2"
+   aAppDir="$( pwd )"                                                                   # .(50610.04.3 Force it)
+
    if [ "${aAppDir/_}" != "${aAppDir}" ]; then aStage="${aAppDir/*_}";aAppDir="${aAppDir/_*/}"; fi  # .(50402.15.2 RAM Split at _)
 
                                        aRepo="$1";                  aAppDir="$1"        # .(50402.15.x RAM Seems strange, but ...)
@@ -52,7 +54,7 @@ exit_wCR() {                                                                    
 
    export aQuiet=q                                                                      # .(50105.05b.7 RAM Add aQuiet)
 
-#  echo -e "\n  aRepo: ${aRepo}', \$2: '$2'"; # exit
+   echo -e "\n  aRepo: ${aRepo}', \$2: '$2'"; # exit
 #  echo "  frt install '${aRepo}' ''           ''  '$3' -d;"; # exit
 #  echo "  frt install '${aRepo}' '${aAppDir}' ''  '$3' -d;"; # exit
 #          frt install  ${aRepo}  $ {aAppDir}  ''   $3  -d;                             # .(50105.05b.8 RAM Remove -q)
@@ -63,9 +65,9 @@ exit_wCR() {                                                                    
 #          frt clone    ${aRepo}  ''  ${aAppDir}  ''  ${aAccount}    ;   # exit
 #          frt clone    ${aRepo}  ''  ${aAppDir}  ''  ${aAccount}  -b;     exit
 
-#  trap    cleanup SIGINT SIGTERM SIGHUP  # Set trap to catch SIGINT                    ##.(50408.03.5).(50610.04.3)
+#  trap    cleanup SIGINT SIGTERM SIGHUP  # Set trap to catch SIGINT                    ##.(50408.03.5).(50610.04.4)
 
-#          frt clone    ${aRepo}  ''  ${aAppDir}  ''  ${aAccount}  -d;   # exit         ##.(50610.04.4 RAM Don't reinstall)
+#          frt clone    ${aRepo}  ''  ${aAppDir}  ''  ${aAccount}  -d;   # exit         ##.(50610.04.5 RAM Don't reinstall)
 
    if [ $? !=  0  ]; then exit 1; fi                                                    # .(50408.03.6)
 
@@ -124,6 +126,7 @@ exit_wCR() {                                                                    
    if [ ! -d "AnyLLM" ]; then bAnyLLM=1; fi                                             # .(50402.15.6)
 
 # --------------------------------------------------------------
+      aOriginalRepoDir=${aRepoDir}; aRepoDir="."                                        # .(50610.04.6)
 
 if [ -d "${aRepoDir}/client1" ]; then
 
@@ -131,7 +134,7 @@ if [ -f "${aRepoDir}/._2/package.json" ]; then                                  
    echo -e "  cd ${aRepoDir}/._2";  # exit                                              # .(50106.06.6).(50104.01.10)
      cd "${aRepoDir}/._2"
            npm install >/dev/null
-     cd ../../
+     cd ../                                                                             # .(50610.04.7 RAM only go up one level)
      fi
 #   --------------------------------------------------
 
@@ -139,7 +142,7 @@ if [ -f "${aRepoDir}/client1/package.json" ]; then
    echo -e "  cd ${aRepoDir}/client1" # exit                                            # .(50106.06.7).(50104.01.11)
      cd "${aRepoDir}/client1"                                                           # .(50106.06.8)
            npm install >/dev/null
-     cd ../../
+     cd ../                                                                             # .(50610.04.8)
      fi # eif ${aRepoDir}/client1/package.json
 #   --------------------------------------------------
 
@@ -148,7 +151,7 @@ if [ -f "${aRepoDir}/server1/package.json" ]; then
      cd "${aRepoDir}/server1"
    echo "  npm install"
            npm install | awk '{ print "    " $0 }'
-     cd ../../                                                                          # .(50406.01c.1 RAM Return back to root dir)
+     cd ../                                                                             # .(50610.04.9).(50406.01c.1 RAM Return back to root dir)
      fi                                                                                 # .(50402.20.1 End)
 # --------------------------------------------------------------
 
@@ -156,31 +159,33 @@ function cpyEnv() {                                                             
       aFrom="$1/$2"; aTo="$1/$3"
 #     echo "    Copying .env file from ${aFrom}"
 #     echo "                      into ${aTo}"
-      if [ ! -f "${aFrom}" ]; then echo     "  * Can't copy .env file: '${aFrom}'";  return; fi             # .(50406.01d.1)
+      if [ ! -f "${aFrom}" ]; then echo     "  * Can't copy .env file: '${aFrom}'";  return; fi                   # .(50406.01d.1)
       echo "    Copying .env file from ${aFrom}  to  .env"                                                  # .(50410.01.1)
       if [   -f "${aFrom}" ]; then            cp -p "${aFrom}" "${aTo}";               fi
       if [ ! -f "${aTo}"   ]; then echo  "  * Copy to: '${aTo}' failed";       return; fi                   # .(50406.01d.2)
       }                                                                                                     # .(50406.03.1 End)
 # --------------------------------------------------------------
 
-                                                              bModelTester=1            # .(50610.04.5 RAM Why wasn't it set?)
+                                                              bModelTester=1            # .(50610.04.10 RAM Why wasn't it set?)
  if [ "${aStage}" == "" ] || [ "${aStage}" == "demo1" ]; then bModelTester=0; fi        # .(50406.01d.1)
 
 #if [  -f "${aRepoDir}/client1/c16_aidocs-review-app/utils/FRTs/_env_local-local.txt" ]; then
 #   cp -p "${aRepoDir}/client1/c16_aidocs-review-app/utils/FRTs/_env_local-local.txt"  "${aRepos}/client1/c16_aidocs-review-app/utils/FRTs/_env"
 #   fi
    echo ""                                                                              # .(50406.01e.1)
-   cpyEnv "./${aRepoDir}/client1/c16_aidocs-review-app/utils/FRTs"  "_env_local-local.txt"  "_env"          # .(50406.03.2 RAM Copy c16 _env)
+#  cpyEnv "./${aRepoDir}/client1/c16_aidocs-review-app/utils/FRTs"  "_env_local-local.txt"  "_env"          ##.(50406.03.2 RAM Copy c16 _env).(50610.04.11)
+   cpyEnv   "${aRepoDir}/client1/c16_aidocs-review-app/utils/FRTs"  "_env_local-local.txt"  "_env"          # .(50610.04.11 RAM Remove ".")
 
 #if [ "${aStage}" == "dev01" ] || [ "${aStage}" == "test1" ]; then                                          # .(50406.03c.1 RAM Try again).(50406.03b.1 RAM Only copy dev01's env)
 
  if [ "${bModelTester}" != "0" ]; then                                                                      # .(50406.03c.1 RAM Try again).(50406.03b.1 RAM Only copy dev01's env)
-   cpyEnv "./${aRepoDir}/server/s01_search-app"      ".env_example"  ".env"                                 # .(50518.01.7)            
-   cpyEnv "./${aRepoDir}/server1/s11_search-mod-app" ".env_example"  ".env"                                 # .(50518.01.8 RAM Rename).(50410.01.2)
-   cpyEnv "./${aRepoDir}/server1/s12_search-web-app" ".env_example"  ".env"                                 # .(50410.01.3).(50406.03.3 RAM Copy s12 .env)
-   cpyEnv "./${aRepoDir}/server1/s13_search-rag-app" ".env_example"  ".env"                                 # .(50410.01.4)
-   cpyEnv "./${aRepoDir}/server1/s14_grading-app"    ".env_example"  ".env"                                 # .(50510.05.1)
-   cd ${aRepoDir}                                                                                           # .(50505.10.1)
+   cpyEnv   "${aRepoDir}/server/s01_search-app"      ".env_example"  ".env"                                 # .(50610.04.12).(50518.01.7)
+   cpyEnv   "${aRepoDir}/server1/s11_search-mod-app" ".env_example"  ".env"                                 # .(50610.04.13).(50518.01.8 RAM Rename).(50410.01.2)
+   cpyEnv   "${aRepoDir}/server1/s12_search-web-app" ".env_example"  ".env"                                 # .(50610.04.14).(50410.01.3).(50406.03.3 RAM Copy s12 .env)
+   cpyEnv   "${aRepoDir}/server1/s13_search-rag-app" ".env_example"  ".env"                                 # .(50610.04.15).(50410.01.4)
+   cpyEnv   "${aRepoDir}/server1/s14_scoring-app"    ".env_example"  ".env"                                 # .(50610.04.16).(50602.02c.1 RAM Change grading to scoring).(50510.05.1)
+
+#  cd ${aRepoDir}                                                                                           # .(50610.04.17 RAM We are already here).(50505.10.1)
    bash set-aidocs.sh doit                                                                                  # .(50505.10.2)
    fi                                                                                                       # .(50406.03b.2)
 # --------------------------------------------------------------
@@ -203,6 +208,8 @@ function cpyEnv() {                                                             
 
  if [ "${bModelTester}" != "0" ]; then                                                  # .(50406.01d.2).(50406.01c.2).(50408.01.x).(50402.15.7 RAM Only set ANYLLM_KEY id it's installed)
    echo -e "//  ------  End of Install  ----------------------------------------------------------------------------- \\\n"       # .(50516.01.1 RAM Add End of Install msg)
+
+   aRepoDir=${aOriginalRepoDir}                                                         # .(50610.04.18)
 
 #  echo -e "\n  Run the AI model testr in any of the server1 app folders, for example"  ##.(50406.01e.3 RAM Remove CR).(50516.02.7)
 #  echo -e "\n  Run the AI model testr commands in the repository, for example:"        ##.(50406.01e.3 RAM Remove CR).(50516.02.7)
@@ -233,7 +240,7 @@ function cpyEnv() {                                                             
    fi                                                                                   # .(50406.01b.1 End).(50406.01.1 End).(50106.04.16 RAM Exit if bDoit=0)
 
  else                                                                                   # .(50402.15.8)
-   echo -e "* AIDocs didn't get installed into folder: ${aRepoDir}.";                   # .(50610.04.6 RAM Remove /client1).(50402.15.9)
+   echo -e "* AIDocs didn't get installed into folder: ${aRepoDir}.";                   # .(50610.04.19 RAM Remove /client1.(50402.15.9)
 #  exit
    fi # eif ${aRepoDir}/client1
  fi  # eif $? -ne 1                                                                     # .(50402.15.10)
